693fbb380c5e4e4e09a0f2bdb2364cb2df75ff34
SumburovSN <44998227+SumburovSN@users.noreply.github.com>
2020-04-15 20:31:37 +0400

    Update Rules.py

--- Rules.py
+++ Rules.py
@@ -3,6 +3,6 @@ rules = '''
 2. The task is to figure it out by typing the probable number (attempt).
 3. The attempt must comprise 4 different numeric symbols.
 4. If the attempt contains the same numeric symbol as the riddle at the same place like in the riddle, one bull (B) is added to the response.
-4. If the attempt contains the same numeric symbol as the riddle but at the different place compared to the riddle, one cow (C) is added to the response.
-5. As a result, the response may contain no more than 4 bulls and cows together. The response of 4 bulls (B4) means that the riddle has been recognized.
+5. If the attempt contains the same numeric symbol as the riddle but at the different place compared to the riddle, one cow (C) is added to the response.
+6. As a result, the response may contain no more than 4 bulls and cows together. The response of 4 bulls (B4) means that the riddle has been recognized.
 '''
